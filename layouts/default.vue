347f1b79e42ca59c09347a7f71479f8a8f4e6133
Christian Lisle <lisleachristian@gmail.com>
2021-12-19 21:41:16 -0600

    Added meta data to default template

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,21 @@
   <Nuxt />
 </template>
 
+<script>
+export default {
+  head() {
+    return {
+      title: 'Christian Lisle',
+      meta: [{
+        hid: 'description',
+        name: 'description',
+        content: "Software engineer passionate about personal projects, open source, and all things software.",
+      }]
+    };
+  },
+};
+</script>
+
 <style>
 * {
   font-family: 'Poppins', sans-serif;

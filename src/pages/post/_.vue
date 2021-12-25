415e1c86deffa05d01865fba4f09d1e42db03cb7
Christian Lisle <lisleachristian@gmail.com>
2021-12-25 00:02:06 -0600

    (WIP) Created single post page

---
+++ src/pages/post/_.vue
@@ -0,0 +1,86 @@
+<template>
+  <main>
+    <nav-bar current-page="Blog" />
+    <div v-if="post" class="max-w-screen-lg mx-auto flex bg-gray-100 m-6 p-4 px-6 flex-wrap shadow-lg hover:shadow-none hover:rounded">
+      <div class="w-full p-4">
+        <p class="text-4xl md:text-5xl lg:text-6xl font-bold mb-2">{{post.title}}</p>
+        <div class="text-gray-600">
+          <p>{{post.date}}
+            <span v-if="post.categories.length">
+              /
+              <span
+                v-for="(category, i) in post.categories"
+                :key="`categories-${category}`"
+              >
+                <a :href="`/category/${category.toLowerCase().replace(' ', '-')}`" class="hover:underline">
+                  {{category}}
+                </a>
+                {{(i + 1) === post.categories.length ? '' : ', '}}
+              </span>
+            </span>
+          </p>
+        </div>
+
+        <img :src="`/blog-images/feature/${post.img}`" class="object-contain w-full md:w-3/4 lg:w-8/12 mx-auto lg:mx-auto max-h-screen" />
+        <nuxt-content :document="post" class="prose max-w-none m-4 mx-auto" />
+      </div>
+    </div>
+
+    <div v-else class="max-w-screen-lg mx-auto flex bg-gray-100 m-6 p-4 px-6 flex-wrap shadow-lg hover:shadow-none hover:rounded animate-pulse">
+      <div class="w-full p-4">
+        <div class="bg-gray-600 w-80 lg:w-96 h-16 mb-2" />
+        <div class="bg-gray-300 w-60 h-4 mb-1" />
+
+      <div class="bg-gray-500 w-full md:w-3/4 lg:w-8/12 mx-auto lg:mx-auto h-96 max-h-screen" />
+      <div v-for="i in 20" :key="i" class="bg-gray-400 w-full h-3 my-2" />
+      <div class="bg-gray-400 w-60 h-3 mb-2"/>
+
+      </div>
+    </div>
+    <Footer current-page="Blog" />
+  </main>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  // eslint-disable-next-line vue/component-definition-name-casing
+  name: 'post',
+  components: {
+    NavBar,
+    Footer,
+  },
+  async asyncData({ $content, params, error }) {
+    const slug = params.pathMatch;
+
+    const posts = await $content('posts')
+      .search('slug', slug)
+      .fetch()
+      .catch((err) => {
+        error({
+          statusCode: 500,
+          message: 'Something went wrong. Please try again.',
+          error: err,
+        });
+      });
+
+    const post = posts?.find((post) => post.slug === slug);
+
+    if (!post) {
+      return error({ statusCode: 404, message: 'Post not found' });
+    }
+
+    return {
+      post
+    };
+  },
+  head() {
+    return {
+      title: this.post.title || 'Christian Lisle',
+    };
+  },
+};
+</script>
+
---
+++ test/pages/post/_.spec.js
@@ -0,0 +1,172 @@
+import { shallowMount } from '@vue/test-utils';
+import Chance from 'chance';
+import generatePost from '../../helpers/postGenerator';
+import post from '@/pages/post/_.vue';
+import NavBar from '@/components/NavBar.vue';
+import Footer from '@/components/Footer.vue';
+
+const chance = new Chance();
+
+describe('post page', () => {
+  let wrapper, fakePost;
+
+  const nuxtContentMock = {
+    '$content': jest.fn().mockReturnThis(),
+    'search': jest.fn().mockReturnThis(),
+    'fetch': jest.fn(),
+  };
+
+  beforeEach(() => {
+    wrapper = shallowMount(post, {
+      stubs: {
+      'nuxt-content': true
+      }
+    });
+  });
+
+  it('is a Vue instance', () => {
+    expect(wrapper.vm).toBeTruthy();
+  });
+
+  it('renders the nav-bar component with the correct props', () => {
+    const navBar = wrapper.findComponent(NavBar);
+    expect(navBar.exists()).toBeTruthy();
+    expect(navBar.props('currentPage')).toEqual('Blog');
+  });
+
+  it('contains the footer component', () => {
+    const footer = wrapper.findComponent(Footer);
+    expect(footer.exists()).toBeTruthy();
+    expect(footer.props('currentPage')).toEqual('Blog'); 
+  });
+
+  describe('given a valid post slug', () => {
+    beforeEach(async () => {
+      fakePost = generatePost();
+
+      nuxtContentMock.fetch.mockImplementation(() => {
+        return {
+          catch: () => [fakePost]
+        };
+      });
+
+      // Source: https://github.com/nuxt/docs/issues/1600#issuecomment-535994612
+      
+      const originalData = post?.data?.() || {};
+      const data = await post.asyncData({
+        $content: () => nuxtContentMock,
+        params: {
+          pathMatch: fakePost.slug,
+        },
+        error: jest.fn(),
+      });
+
+      post.data = () => {
+        return { ...originalData, ...data };
+      };
+
+      wrapper = shallowMount(post, {
+        stubs: {
+          'nuxt-content': true
+        }
+      });
+    });
+
+    afterEach(jest.clearAllMocks);
+
+    it('calls nuxt content search with the correct values', () => {
+      expect(nuxtContentMock.search).toBeCalledTimes(1);
+      expect(nuxtContentMock.search).toBeCalledWith('slug', fakePost.slug);
+    });
+
+    it('calls nuxt content fetch', () => {
+      expect(nuxtContentMock.fetch).toBeCalledTimes(1);
+    });
+
+    it('renders the post title and date correctly', () => {
+      expect(wrapper.text()).toContain(fakePost.title);
+      expect(wrapper.text()).toContain(fakePost.date);
+    });
+
+    it('renders the post categories correctly', () => {
+      expect(wrapper.text()).toContain(fakePost.title);
+      expect(wrapper.text()).toContain(fakePost.date);
+      
+      fakePost.categories.forEach((category) => {
+        expect(wrapper.text()).toContain(category);
+      });
+    });
+
+    it('renders the post feature image correctly', () => {
+      expect(wrapper.html()).toContain(`<img src="/blog-images/feature/${fakePost.img}"`);
+    });
+
+    it('contains nuxt content element for the post body', () => {
+      expect(wrapper.html()).toContain('<nuxt-content-stub');
+    });
+
+    describe('given there is an issue fetching data', () => {
+      let mockErrorFn, fakeError;
+
+      beforeEach(async () => {
+        fakeError = new Error(chance.sentence());
+
+        mockErrorFn = jest.fn();
+
+        nuxtContentMock.fetch.mockImplementation(() => {
+          return {
+            catch: (callback) => callback(fakeError)
+          };
+        });
+
+        await post.asyncData({
+          $content: () => nuxtContentMock,
+          params: {
+            pathMatch: fakePost.slug,
+          },
+          error: mockErrorFn,
+        });
+      });
+
+      it('envokes the nuxt error function correctly', () => {
+        expect(mockErrorFn).toBeCalled();
+        expect(mockErrorFn).toBeCalledWith({
+          statusCode: 500,
+          message: 'Something went wrong. Please try again.',
+          error: fakeError
+        });
+      });
+    });
+  });
+
+  describe('given an invalid post slug (non-existent post)', () => {
+    let mockErrorFn;
+
+    beforeEach(async () => {
+      fakePost = undefined;
+      mockErrorFn = jest.fn();
+
+      nuxtContentMock.fetch.mockImplementation(() => {
+        return {
+          catch: () => fakePost
+        };
+      });
+
+      await post.asyncData({
+        $content: () => nuxtContentMock,
+        params: {
+          pathMatch: chance.string(),
+        },
+        error: mockErrorFn,
+      });
+    });
+
+    it('envokes the nuxt error function corrctly', () => {
+      expect(mockErrorFn).toBeCalled();
+      expect(mockErrorFn).toBeCalledWith({
+        statusCode: 404,
+        message: 'Post not found',
+      });
+    });
+  });  
+});

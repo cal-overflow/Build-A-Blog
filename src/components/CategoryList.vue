1a05911d5ab2d7b5c4cd8ecf22c508dea0c91437
Christian Lisle <lisleachristian@gmail.com>
2022-08-09 15:36:52 -0500

    Improve styling on Category List & Dropdown

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="category-list" :class="`flex flex-col bg-card-light dark:bg-card-dark m-4 shadow-lg dark:shadow-shadow-dark hover:shadow-none hover:rounded motion-safe:animate-fade-in-fast transition ${classes}`">
+  <div id="category-list" :class="`w-48 flex flex-col bg-card-light dark:bg-card-dark mt-4 shadow-lg dark:shadow-shadow-dark hover:shadow-none hover:rounded motion-safe:animate-fade-in-fast transition ${classes}`">
     <nav-item
       v-for="(category, i) in categories"
       :key="category.title"
       :ref="category.slug"
       :href="`/category/${category.slug}`"
-      class="text-center"
+      class="text-center mx-4 transition"
       :style="`animation: fade-in ${((i + 1) * 0.25)}s ease-in-out;`"
       :active="currentPage === category.title"
     >

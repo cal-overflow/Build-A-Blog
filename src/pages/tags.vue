2007d98593df12e37b7ba2e60cd195ac9ef8b4fd
mamoore1440 <mamoore1440@gmail.com>
2022-12-29 19:45:12 -0600

    split tags.vue into tags.vue and TagFeed.vue. Split testing also
    Co-authored-by: cal <cal-overflow@users.noreply.github.com>

--- src/pages/tags.vue
+++ src/pages/tags.vue
@@ -1,71 +1,25 @@
 <template>
   <main>
     <nav-bar current-page="Tags" />
-    <div class="max-w-screen-lg mx-auto">
-      <div class="bg-card-light dark:bg-card-dark m-6 p-6 shadow-md dark:shadow-shadow-dark hover:shadow-none hover:rounded motion-safe:animate-fade-in-fast transition">
-        <p class="text-center text-3xl font-bold">Tags</p>
-      </div>
-      <divider />
-
-      <div v-if="tags && tags.length">
-        <tag-preview
-          v-for="(tag, i) in tags"
-          :key="tag.slug"
-          :tag="tag"
-          :index="i"
-        />
-      </div>
-
-      <div v-else-if="!isDoneFetchingTags">
-        <tag-preview
-          v-for="(n, i) in 3"
-          :key="`tag-lazy-loader-${n}`"
-          :index="i"
-        />
-      </div>
-      
-      <div v-else class="bg-card-light dark:bg-card-dark m-6 p-6 hover:rounded shadow-md dark:shadow-shadow-dark hover:shadow-none motion-safe:animate-fade-in transition col-span-2">
-        <p class="text-center md:text-left text-2xl font-bold mt-2">No tags 😴</p>
-        <p class="text-center md:text-left">There are not currently any tags</p>
-      </div>
-
-    </div>
+    <tag-feed path="tags" />
     <back-to-top-button />
     <footer-bar current-page="Tags" />
   </main>
 </template>
 
 <script>
-import Divider from '@/components/helpers/Divider.vue';
-import TagPreview from '@/components/previews/Tag.vue';
 import NavBar from '@/components/structural/NavBar.vue';
 import FooterBar from '@/components/structural/FooterBar.vue';
 import BackToTopButton from '@/components/helpers/BackToTopButton.vue';
+import TagFeed from '@/components/views/TagFeed.vue';
 
 export default {
   name: 'tags',
   components: {
-    TagPreview,
-    Divider,
     NavBar,
     BackToTopButton,
-    FooterBar
-  },
-  async asyncData({ $content, error }) {
-    const content = await $content('tags')
-      .fetch()
-      .catch((err) => {
-        error({
-          statusCode: 500,
-          message: 'Something went wrong while fetching tags',
-          error: err
-        });
-      });
-
-    return {
-      tags: content.tags,
-      isDoneFetchingTags: true
-    };
+    FooterBar,
+    TagFeed
   },
 };
 </script>
--- test/unit/pages/tags.spec.js
+++ test/unit/pages/tags.spec.js
@@ -1,31 +1,15 @@
-import { shallowMount, RouterLinkStub } from '@vue/test-utils';
-import Chance from 'chance';
-import generateTag from '../../helpers/tagGenerator.js';
+import { shallowMount } from '@vue/test-utils';
 import tags from '@/pages/tags.vue';
-import TagPreview from '@/components/previews/Tag.vue';
 import NavBar from '@/components/structural/NavBar.vue';
 import FooterBar from '@/components/structural/FooterBar.vue';
-import Divider from '@/components/helpers/Divider.vue';
+import TagFeed from '@/components/views/TagFeed.vue';
 
-
-const chance = new Chance();
 
 describe('tags page', () => {
-  let wrapper, fakeTags;
-
-  const stubs = {
-    NuxtLink: RouterLinkStub,
-    'nuxt-content': true,
-  };
-
-  const nuxtContentMock = {
-    $content: jest.fn().mockReturnThis(),
-    where: jest.fn().mockReturnThis(),
-    fetch: jest.fn(),
-  };
+  let wrapper;
 
   beforeEach(() => {
-    wrapper = shallowMount(tags, { stubs });
+    wrapper = shallowMount(tags);
   });
 
   it('is a Vue instance', () => {
@@ -38,145 +22,15 @@ describe('tags page', () => {
     expect(navBar.props('currentPage')).toEqual('Tags');
   });
 
+  it('renders the TagFeed component with the correct props', () => {
+    const tagFeed = wrapper.findComponent(TagFeed);
+    expect(tagFeed.exists()).toBeTruthy();
+    expect(tagFeed.props('path')).toEqual('tags');
+  });
+
   it('contains the FooterBar component', () => {
     const footer = wrapper.findComponent(FooterBar);
     expect(footer.exists()).toBeTruthy();
     expect(footer.props('currentPage')).toEqual('Tags');
-  });
-
-  describe('given the tags have not loaded yet', () => {
-    beforeEach(() => {
-      // mock nuxt content fetch to never resolve (stuck loading behavior)      
-      nuxtContentMock.fetch.mockReturnValue(new Promise(() => ({})));
-
-      wrapper = shallowMount(tags, {
-        mocks: {
-          $content: () => nuxtContentMock
-        },
-        stubs,
-      });
-    });
-
-    it('renders TagPreview components with no post prop', () => {
-      const tagPreviews = wrapper.findAllComponents(TagPreview);
-
-      tagPreviews.wrappers.forEach((wrapper) => {
-        expect(wrapper.props('tag')).toEqual(undefined);
-      });
-    });
-  });
-
-  describe('given there are no tags', () => {
-    beforeEach(async () => {
-      fakeTags = [];
-      nuxtContentMock.fetch.mockResolvedValue({ tags: fakeTags });
-
-      const data = await tags.asyncData({
-        $content: () => nuxtContentMock,
-        error: jest.fn()
-      });
-
-      tags.data = () => (data);
-      wrapper = shallowMount(tags, {
-        mocks: {
-          $content: () => nuxtContentMock
-        },
-        stubs,
-      });
-    });
-
-    it('renders a "no tags" message', () => {
-      expect(wrapper.text()).toContain('There are not currently any tags');
-    });
-  });
-
-  describe('given there are tags', () => {
-    beforeEach(async () => {
-      fakeTags = chance.n(generateTag, chance.integer({
-        min: 1, max: 20
-      }));
-
-      nuxtContentMock.fetch.mockResolvedValue({ tags: fakeTags });
-      const data = await tags.asyncData({
-        $content: () => nuxtContentMock,
-        error: jest.fn()
-      });
-
-      tags.data = () => (data);
-      wrapper = shallowMount(tags, {
-        mocks: {
-          $content: () => nuxtContentMock
-        },
-        stubs,
-      });
-    });
-
-    afterEach(jest.clearAllMocks);
-
-    it('is a Vue instance', () => {
-      expect(wrapper.vm).toBeTruthy();
-    });
-
-    it('contains the correct text', () => {
-      expect(wrapper.text()).toContain("Tags");
-    });
-
-    it('contains a Divider component', () => {
-      expect(wrapper.findComponent(Divider).exists()).toBeTruthy();
-    });
-
-
-    it('calls nuxt content fetch', () => {
-      expect(nuxtContentMock.fetch).toBeCalledTimes(1);
-    });
-
-    it('renders a component for each tag', () => {
-      fakeTags.forEach((tag) => {
-        expect(wrapper.findComponent({ ref: tag.slug })).toBeTruthy();
-      });
-    });
-
-    it('renders the correct number of TagPreview components', () => {
-      expect(wrapper.findAllComponents(TagPreview).wrappers).toHaveLength(fakeTags.length);
-    });
-  });
-
-  describe('given there is an issue fetching tags', () => {
-    let mockErrorFn, fakeError;
-
-    beforeEach(async () => {
-      fakeTags = [];
-      fakeError = new Error(chance.sentence());
-      mockErrorFn = jest.fn();
-      
-      nuxtContentMock.fetch.mockImplementation(() => {
-        return {
-          catch: (callback) => {
-            callback(fakeError);
-            return fakeTags;
-          }
-        };
-      });
-
-      wrapper = shallowMount(tags, {
-        stubs: {
-          'nuxt-content': true
-        }
-      });
-
-      await tags.asyncData({
-        $content: () => nuxtContentMock,
-        error: mockErrorFn,
-      });
-    });
-
-    it('envokes the nuxt error function correctly', () => {
-      expect(mockErrorFn).toBeCalled();
-      expect(mockErrorFn).toBeCalledWith({
-        statusCode: 500,
-        message: 'Something went wrong while fetching tags',
-        error: fakeError
-      });
-    });
   });
 });

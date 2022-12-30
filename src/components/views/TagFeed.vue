3af251638b9b264d7356ffbc6b51ae1ca5fe3475
Michael Moore <83515252+mamoore1440@users.noreply.github.com>
2022-12-29 19:52:19 -0600

    split tags.vue into tags.vue and TagFeed.vue. Split testing also (#153)
    Co-authored-by: cal <cal-overflow@users.noreply.github.com>
    
    Co-authored-by: cal <cal-overflow@users.noreply.github.com>

---
+++ src/components/views/TagFeed.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="max-w-screen-lg mx-auto">
+    <div
+      class="bg-card-light dark:bg-card-dark m-6 p-6 shadow-md dark:shadow-shadow-dark hover:shadow-none hover:rounded motion-safe:animate-fade-in-fast transition">
+      <p class="text-center text-3xl font-bold">Tags</p>
+    </div>
+    <divider />
+
+    <div v-if="tags && tags.length">
+      <tag-preview v-for="(tag, i) in tags" :key="tag.slug" :tag="tag" :index="i" />
+    </div>
+
+    <div v-else-if="!isDoneFetchingTags">
+      <tag-preview v-for="(n, i) in 3" :key="`tag-lazy-loader-${n}`" :index="i" />
+    </div>
+
+    <div v-else
+      class="bg-card-light dark:bg-card-dark m-6 p-6 hover:rounded shadow-md dark:shadow-shadow-dark hover:shadow-none motion-safe:animate-fade-in transition col-span-2">
+      <p class="text-center md:text-left text-2xl font-bold mt-2">No tags 😴</p>
+      <p class="text-center md:text-left">There are not currently any tags</p>
+    </div>
+  </div>
+</template>
+  
+<script>
+import Divider from '@/components/helpers/Divider.vue';
+import TagPreview from '@/components/previews/Tag.vue';
+
+export default {
+  name: 'tags-feed',
+  components: {
+    TagPreview,
+    Divider,
+  },
+  props: {
+    path: {
+      default: undefined,
+      type: String,
+      required: true,
+    },
+  },
+  data: () => ({
+    tags: [],
+    isDoneFetchingTags: false,
+  }),
+  async fetch() {
+    const content = await this.$content(this.path)
+      .fetch()
+      .catch((err) => {
+        this.$error({
+          statusCode: 500,
+          message: 'Something went wrong while fetching tags',
+          error: err
+        });
+      });
+
+    this.tags = content.tags;
+    this.isDoneFetchingTags = true;
+  },
+};
+</script>
---
+++ test/unit/components/TagFeed.spec.js
@@ -0,0 +1,170 @@
+import { mount, RouterLinkStub } from '@vue/test-utils';
+import Chance from 'chance';
+import generateTag from '../../helpers/tagGenerator.js';
+import TagFeed from '@/components/views/TagFeed.vue';
+import TagPreview from '@/components/previews/Tag.vue';
+import Divider from '@/components/helpers/Divider.vue';
+
+const chance = new Chance();
+
+describe('TagFeed component', () => {
+  let wrapper, fakePath, fakeTags;
+
+  const stubs = {
+    NuxtLink: RouterLinkStub,
+    'nuxt-content': true,
+  };
+
+  const nuxtContentMock = {
+    $content: jest.fn().mockReturnThis(),
+    where: jest.fn().mockReturnThis(),
+    sortBy: jest.fn().mockReturnThis(),
+    fetch: jest.fn(),
+  };
+
+
+  beforeEach(() => {
+    fakePath = `/${chance.string({ pool: 'abcdef' })}`;
+    wrapper = mount(TagFeed, {
+      mocks: {
+        $content: () => nuxtContentMock
+      },
+      propsData: {
+        path: fakePath
+      },
+      stubs
+    });
+  });
+
+  it('is a Vue instance', () => {
+    expect(wrapper.vm).toBeTruthy();
+  });
+
+  describe('given the tags have not loaded yet', () => {
+    beforeEach(() => {
+      // mock nuxt content fetch to never resolve (stuck loading behavior)      
+      nuxtContentMock.fetch.mockReturnValue(new Promise(() => ({})));
+
+      wrapper = mount(TagFeed, {
+        mocks: {
+          $content: () => nuxtContentMock
+        },
+        stubs,
+      });
+    });
+
+    it('renders TagPreview components with no post prop', () => {
+      const tagPreviews = wrapper.findAllComponents(TagPreview);
+
+      tagPreviews.wrappers.forEach((wrapper) => {
+        expect(wrapper.props('tag')).toEqual(undefined);
+      });
+    });
+  });
+
+  describe('given there are no tags', () => {
+    beforeEach(() => {
+      fakeTags = [];
+      wrapper = mount(TagFeed, {
+        data: () => ({
+          tags: [],
+          isDoneFetchingTags: true
+        }),
+        mocks: {
+          $content: () => nuxtContentMock
+        },
+        stubs,
+      });
+    });
+
+    it('renders a "no tags" message', () => {
+      expect(wrapper.text()).toContain('There are not currently any tags');
+    });
+  });
+
+  describe('given there are tags', () => {
+    beforeEach(() => {
+      fakeTags = chance.n(generateTag, chance.integer({
+        min: 1, max: 20
+      }));
+
+      nuxtContentMock.fetch.mockResolvedValue({ tags: fakeTags });
+
+      wrapper = mount(TagFeed, {
+        data: () => ({
+          tags: fakeTags,
+        }),
+        mocks: {
+          $content: () => nuxtContentMock
+        },
+        stubs,
+      });
+    });
+
+    afterEach(jest.clearAllMocks);
+
+    it('is a Vue instance', () => {
+      expect(wrapper.vm).toBeTruthy();
+    });
+
+    it('contains the correct text', () => {
+      expect(wrapper.text()).toContain("Tags");
+    });
+
+    it('contains a Divider component', () => {
+      expect(wrapper.findComponent(Divider).exists()).toBeTruthy();
+    });
+
+    it('renders a component for each tag', () => {
+      fakeTags.forEach((tag) => {
+        expect(wrapper.findComponent({ ref: tag.slug })).toBeTruthy();
+      });
+    });
+
+    it('renders the correct number of TagPreview components', () => {
+      expect(wrapper.findAllComponents(TagPreview).wrappers).toHaveLength(fakeTags.length);
+    });
+  });
+
+  // describe('given there is an issue fetching tags', () => {
+  //   let mockErrorFn, fakeError;
+
+  //   beforeEach(async () => {
+  //     fakeTags = [];
+  //     fakeError = new Error(chance.sentence());
+  //     mockErrorFn = jest.fn();
+
+  //     nuxtContentMock.fetch.mockImplementation(() => {
+  //       return {
+  //         catch: (callback) => {
+  //           callback(fakeError);
+  //           return fakeTags;
+  //         }
+  //       };
+  //     });
+
+  //     wrapper = mount(TagFeed, {
+  //       data: () => ({
+  //         tags: [],
+  //       }),
+  //       mocks: {
+  //         $content: () => nuxtContentMock
+  //       },
+  //       stubs: {
+  //         'nuxt-content': true
+  //       }
+  //     });
+  //     await TagFeed.fetch();
+
+  //   });
+
+  //   it('invokes the nuxt error function correctly', () => {
+  //     expect(mockErrorFn).toBeCalled();
+  //     expect(mockErrorFn).toBeCalledWith({
+  //       statusCode: 500,
+  //       message: 'Something went wrong while fetching tags',
+  //       error: fakeError
+  //     });
+  //   });
+  // });
+});

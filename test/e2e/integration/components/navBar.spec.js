5dbe50d292076d9171f558d8d70d26e7521103fd
Christian Lisle <lisleachristian@gmail.com>
2023-01-04 20:27:27 -0600

    Remove old e2e tests for tags dropdown

--- test/e2e/integration/components/navBar.spec.js
+++ test/e2e/integration/components/navBar.spec.js
@@ -24,23 +24,4 @@ describe('Nav bar', () => {
     cy.contains('Portfolio').click();
     cy.location('pathname').should('eq', '/tags/portfolio');
   });
-
-  describe('Tags dropdown', () => {
-    beforeEach(() => {
-      cy.contains('Tags').trigger('mouseenter');
-      cy.wait(500);
-    });
-
-    it('shows the dropdown on mouseenter event', () => {
-      cy.get('#tag-list').should('be.visible');
-    });
-    
-    it('stops showing the dropdown on mouseleave event', () => {
-      cy.get('#tag-list').should('be.visible');
-
-      cy.contains('Tags').trigger('mouseleave');
-      cy.wait(500);
-      cy.get('#tag-list').should('not.be.visible');
-    });
-  });
 });

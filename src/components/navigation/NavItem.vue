f0b3da576bc9780335b546941d2256d403f1b1a8
Christian Lisle <lisleachristian@gmail.com>
2023-01-18 12:22:47 -0600

    Add support for external lines in `<NavItem />`

--- src/components/navigation/NavItem.vue
+++ src/components/navigation/NavItem.vue
@@ -1,5 +1,13 @@
 <template>
+  <a
+    v-if="href.startsWith('http://') || href.startsWith('https://')"
+    :href="href"
+    :class="`p-3 hover:underline align-middle transition ${active ? 'text-primary-light dark:text-primary-dark' : 'dark:text-white'}`"
+  >
+    <slot />
+  </a>
   <nuxt-link
+    v-else
     :to="href"
     :class="`p-3 hover:underline align-middle transition ${active ? 'text-primary-light dark:text-primary-dark' : 'dark:text-white'}`"
   >

de7545a6e1d5c36fe769a61ef3d13ff3e62bf181
Christian Lisle <lisleachristian@gmail.com>
2023-02-11 18:34:01 -0600

    Fix invalid use of sed

--- template/deploy.sh
+++ template/deploy.sh
@@ -14,7 +14,7 @@ npm ci
 npm run generate
 
 
-sed "/<head>/a $(cat template/google-analytics-tag.html)" dist/index.html > temp.html && mv temp.html dist/index.html
+sed '/<head>/r template/google-analytics-tag.html' dist/index.html > temp.html && mv temp.html dist/index.html
 
 BUCKET=$(aws cloudformation describe-stacks --stack-name=$STACK_NAME --region $REGION --query 'Stacks[0].Outputs[?OutputKey==`S3BucketName`].OutputValue' --output text)
 aws s3 sync dist s3://$BUCKET

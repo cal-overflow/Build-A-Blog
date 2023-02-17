45d1c21d6295048d2aad533ce29e35fd2db1dcd6
Christian Lisle <lisleachristian@gmail.com>
2023-02-17 16:57:41 -0600

    Add support for new socials in footerbar

--- src/components/structural/FooterBar.vue
+++ src/components/structural/FooterBar.vue
@@ -19,6 +19,22 @@
         <a v-if="content.imageNavItems && content.imageNavItems.githubUrl" id="github-link" :href="content.imageNavItems.githubUrl" target="_blank" title="View my Github account">
           <img id="github-logo" class="invert aspect-square w-6 h-auto inline align-middle mx-4" src="~/assets/images/github.png" />
         </a>
+
+        <a v-if="content.imageNavItems && content.imageNavItems.twitterUrl" id="twitter-link" :href="content.imageNavItems.twitterUrl" target="_blank" title="View my Twitter account">
+          <img id="twitter-logo" class="invert aspect-square w-6 h-auto inline align-middle mx-4" src="~/assets/images/twitter.png" />
+        </a>
+
+        <a v-if="content.imageNavItems && content.imageNavItems.facebookUrl" id="facebook-link" :href="content.imageNavItems.facebookUrl" target="_blank" title="View my Facebook account">
+          <img id="facebook-logo" class="invert aspect-square w-6 h-auto inline align-middle mx-4" src="~/assets/images/facebook.png" />
+        </a>
+
+        <a v-if="content.imageNavItems && content.imageNavItems.linkedinUrl" id="linkedin-link" :href="content.imageNavItems.linkedinUrl" target="_blank" title="View my LinkedIn account">
+          <img id="linkedin-logo" class="invert aspect-square w-6 h-auto inline align-middle mx-4" src="~/assets/images/linkedin.png" />
+        </a>
+
+        <a v-if="content.imageNavItems && content.imageNavItems.instagramUrl" id="instagram-link" :href="content.imageNavItems.instagramUrl" target="_blank" title="View my Instagram account">
+          <img id="instagram-logo" class="invert aspect-square w-6 h-auto inline align-middle mx-4" src="~/assets/images/instagram.png" />
+        </a>
       </div>
 
     </nav>

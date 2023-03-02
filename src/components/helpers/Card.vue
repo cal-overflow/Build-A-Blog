48317fe1536f8c9a18278126fd130cfe7f377599
Christian Lisle <lisleachristian@gmail.com>
2023-03-01 21:29:25 -0600

    Add to other nuxt-content components

--- src/components/helpers/Card.vue
+++ src/components/helpers/Card.vue
@@ -2,7 +2,7 @@
   <div class="grow page-preview-card flex col-span-1 flex-col justify-between bg-card-light dark:bg-card-dark text-center p-6 w-full hover:rounded shadow-md dark:shadow-shadow-dark hover:shadow-none transition">
     <nuxt-content
       :document="content"
-      class="w-full max-w-none prose prose:dark-invert dark:prose-invert leading-normal transition"
+      class="w-full max-w-none prose prose:dark-invert dark:prose-invert leading-normal prose-code:before:content-none prose-code:after:content-none transition"
     />
   </div>
 </template>

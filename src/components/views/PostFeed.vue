e6e745547a024debba59a7c82245ba1f912b48b4
Christian Lisle <lisleachristian@gmail.com>
2023-03-12 13:22:06 -0500

    Fix styling issues from latest PR (filter)

--- src/components/views/PostFeed.vue
+++ src/components/views/PostFeed.vue
@@ -3,8 +3,11 @@
     <div class="max-w-screen-lg mx-auto">
       <div class="bg-card-light dark:bg-card-dark m-6 p-6 shadow-md dark:shadow-shadow-dark hover:shadow-none hover:rounded motion-safe:animate-fade-in-fast transition">
         <div class="flex sm:justify-between text-center sm:text-left">
+          <!-- A spacer button for centering the title -->
+          <div class="block sm:hidden w-6 h-6 opacity-0 cursor-default">
+          </div>
           <p class="grow text-3xl font-bold">{{metadata.title}}</p>
-          <button class="text-white rounded-lg" @click="isShowingFilter = !isShowingFilter">
+          <button class="dark:text-white rounded-lg" @click="isShowingFilter = !isShowingFilter">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
             </svg>
